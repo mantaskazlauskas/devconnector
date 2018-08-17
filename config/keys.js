3594d1fe0ca1bad7fdaf0d1d27b661dc05bb905e
Mantas <mmantas.kazlauskas@gmail.com>
2018-08-17 23:41:25 +0300

    JWT tokens added to login

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://mantas:behemoth7@ds123852.mlab.com:23852/devconnector1991"
+    "mongodb://mantas:behemoth7@ds123852.mlab.com:23852/devconnector1991",
+  secretOrKey: "secret"
 };
